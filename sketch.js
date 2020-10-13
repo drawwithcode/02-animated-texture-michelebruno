d239777f78e5b12e4c3fd9fdd41280ffaaf987f3
michelebruno <bm.michelebruno@gmail.com>
2020-10-13 23:58:37 +0200

    Maybe final commit

--- sketch.js
+++ sketch.js
@@ -1,58 +1,55 @@
-let cols, rows, fr, bg,
-    scaleX = 70,
-    scaleY = 26,
+let cols, rows, bg, howTo,
+    scaleX = 100,
+    scaleY = 46,
     zoff = 0,
-    strkW = 6,
+    strkW = 5,
     rainbowMode = true,
-    patternIndex = 0;
+    patternIndex = 1,
+    increment = 0.07;
 
-const increment = 0.02,
-    PALETTE = [],
+const cells = [],
     PATTERNS = [
         ({w, h, col} = {}) => {
-
             push();
 
             if (!rainbowMode || typeof col === "undefined") {
-                fill(PALETTE[4])// fill(PALETTE[1])
+                fill(color(235))
             }
 
             rectMode(CENTER);
-
-            rect(scaleX / 2, scaleY / 2, w, h);
+            rect(scaleX / 2, scaleY / 2, scaleX - 2 * strkW, h);
 
             pop();
         },
-        CHECKERED = ({w, h} = {}) => {
+        WAVE = ({col, h} = {}) => {
             push();
 
-            translate(strkW, strkW);
+            h *= 1.1;
 
-            let s = 5,
-                c = floor((w - 2 * strkW) / s),
-                r = floor((h - 2 * strkW) / s);
+            let w = scaleX;
 
-            noStroke();
+            stroke(col);
+            strokeWeight(3)
 
-            for (let y = 0; y < r; y++) {
-                for (let x = 0; x < c; x++) {
-                    if ((x % 2 === 0 && y % 2 === 1) || (x % 2 === 1 && y % 2 === 0)) {
-                        rect(x * s, y * s, w / 6, s);
-                    }
-                }
+            for (let i = 0; i < 3; i++) {
+                translate(0, cos(frameCount / 5) + h / 4);
+                bezier(0, 0, w * .2, 0, w * .2, h * .6, w / 2, h * .6);
+                bezier(w / 2, h * .6, w * .8, h * .6, w * .8, 0, w, 0);
             }
 
             pop();
         },
-    ],
-    cells = [];
+    ];
+
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    let canvas = createCanvas(windowWidth, windowHeight);
 
-    PALETTE.push(color(20), color(10, 255, 0), color(255, 255, 0), color(255, 0, 0), color(235));
+    canvas.parent('#bg');
 
-    bg = PALETTE[0];
+    frameRate(15)
+
+    bg = color(20)
 
     scaleX += strkW * 2;
     scaleY += strkW * 2;
@@ -63,23 +60,20 @@ function setup() {
 
     strokeWeight(strkW)
 
-    cols = floor(width / scaleX) - 2;
-    rows = floor(height / scaleY);
-
-    fr = createP('');
+    cols = floor(width / scaleX) - 1;
+    rows = floor(height / scaleY) - 1;
 
     for (let y = 0; y < rows; y++) {
         for (let x = 0; x < cols; x++) {
-            cells.push(new Cell({x: x * scaleX, y: y * scaleY}))
+            cells.push(new Cell({x: x * scaleX, y: y * scaleY, cellCol: x, cellRow: y}))
         }
-
     }
 }
 
 function draw() {
     background(bg)
 
-    translate(width % scaleX, height % scaleY);
+    translate(width % scaleX / 2, height % scaleY);
 
     let yoff = 0;
 
@@ -94,21 +88,18 @@ function draw() {
                     ? lerpColor(color(255, 0, 0), color(0, 0, 255), n2)
                     : lerpColor(color(0, 0, 255), color(0, 255, 255), n2 - 1);
 
-            // console.log(col, n2, x, y, n)
             c.run({
                 col,
                 widthRatio: map(n, 0, 1, 0.1, 1),
-                heightRatio: map(n, 0, 1, 0.1, 1),
+                heightRatio: map(n, 0, 1, 0.4, 1),
             });
             xoff += increment;
         }
 
         yoff += increment;
     }
-    zoff += increment;
 
-
-    fr.html(floor(frameRate()));
+    zoff += increment /2;
 
 }
 
@@ -119,35 +110,40 @@ function Cell(
         col,
         cellWidth = scaleX,
         cellHeight = scaleY,
+        cellRow,
+        cellCol
     } = {}
 ) {
 
     this.x = x;
     this.y = y;
+    this.row = cellRow;
+    this.col = cellCol;
     this.color = typeof col !== 'undefined' ? col : color(230);
     this.height = cellHeight;
     this.width = cellWidth;
     this.initialHeight = cellHeight;
     this.initialWidth = cellWidth;
 
-
     this.draw = function () {
         push();
         fill(this.color)
         translate(this.x, this.y);
+        if (patternIndex === 1 && this.row % 2) translate(scaleX / 4, 0);
         this.pattern({w: this.width, h: this.height, col: this.color});
         pop();
     }
 
     this.run = function ({col, width, widthRatio, heightRatio}) {
 
-        this.pattern = PATTERNS[0];
+        this.pattern = PATTERNS[patternIndex % PATTERNS.length];
 
         if (typeof width !== "undefined") {
             this.width = width;
         } else if (typeof widthRatio !== "undefined") {
             this.width = this.initialWidth * widthRatio;
         }
+
         if (typeof heightRatio !== "undefined") {
             this.height = this.initialHeight * heightRatio;
         }
@@ -159,19 +155,20 @@ function Cell(
     }
 }
 
-
-function getNoisedColor(n1 = 1, n2 = 1, n3 = abs(cos(frameCount / 10))) {
-    return color(255 - n1 * 255, 255 - 255 * n2, 255 * n3);
-}
-
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 
 function mousePressed() {
-    rainbowMode = !rainbowMode;
+    // rainbowMode = !rainbowMode;
 
-    // patternIndex++;
+    patternIndex++;
+
+    patternIndex = patternIndex % PATTERNS.length;
+
+    if (patternIndex === 1) {
+        increment = 0.03;
+    } else increment = 0.07
 
     // patternIndex = patternIndex % PATTERNS.length;
 }
